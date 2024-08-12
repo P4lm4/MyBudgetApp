4b2eacb23c34a7da6fedb1d1876ac849ffd76852
Igor Pavlović <pigorp@hotmail.com>
2024-08-12 20:25:45 +0200

    Initial commit

---
+++ src/test/java/com/mybudget/igor/MybudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybudget.igor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
