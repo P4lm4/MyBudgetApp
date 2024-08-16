3fa00222164514325adba962748832b02f4c6cc8
Igor Pavlović <pigorp@hotmail.com>
2024-08-16 11:27:44 +0200

    Updated function getAllCurrency save data and update them every 24 hours.

--- src/main/java/com/mybudget/igor/service/CurrencyService.java
+++ src/main/java/com/mybudget/igor/service/CurrencyService.java
@@ -8,38 +8,57 @@ import org.springframework.stereotype.Service;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
 import java.net.URL;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Scanner;
 
 @Service
 public class CurrencyService {
+
+    HashMap<String,String> allCurrenciesInfo;
+    HashMap<String, Double> exchangeRateInfo;
+    LocalDateTime lastUpdate = LocalDateTime.of(2024, 8, 14, 12, 0, 0);
+
+
+
+
+
+
     public HashMap<String, String> getAllCurrencies() {
-        try {
-            URL urlObj = new URL("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.min.json");
-            HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
-            connection.setRequestMethod("GET");
+        if(allCurrenciesInfo == null || Duration.between(lastUpdate, LocalDateTime.now()).toHours() >= 24) {
+            try {
+                URL urlObj = new URL("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.min.json");
+                HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
+                connection.setRequestMethod("GET");
 
-            int responseCode = connection.getResponseCode();
+                int responseCode = connection.getResponseCode();
 
-            if (responseCode == HttpsURLConnection.HTTP_OK) {
-                StringBuilder sb = new StringBuilder();
-                Scanner scanner = new Scanner(connection.getInputStream());
-                while (scanner.hasNext()) {
-                    sb.append(scanner.nextLine());
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    StringBuilder sb = new StringBuilder();
+                    Scanner scanner = new Scanner(connection.getInputStream());
+                    while (scanner.hasNext()) {
+                        sb.append(scanner.nextLine());
+                    }
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    setLastUpdate(LocalDateTime.now());
+                    return allCurrenciesInfo = objectMapper.readValue(String.valueOf(sb), new TypeReference<HashMap<String, String>>(){});
+                } else {
+                    System.out.println("Error is sending a GET request");
+                    return null;
                 }
-                ObjectMapper objectMapper = new ObjectMapper();
-                return objectMapper.readValue(String.valueOf(sb), new TypeReference<HashMap<String, String>>(){});
-            } else {
-                System.out.println("Error is sending a GET request");
+            } catch(IOException ex) {
+                System.out.println("Error fetching currencies: " + ex.getMessage());
                 return null;
             }
-        } catch(IOException ex) {
-            System.out.println("Error fetching currencies: " + ex.getMessage());
-            return null;
         }
+        return allCurrenciesInfo;
+
+
     }
 
     public Double getExchangeRate (String currencyFrom, String currencyTo) {
+
         try {
             URL urlObj = new URL("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" + currencyFrom + ".json");
             HttpsURLConnection connection = (HttpsURLConnection) urlObj.openConnection();
@@ -72,5 +91,29 @@ public class CurrencyService {
             System.out.println("Error fetching exchange rate from '" + currencyFrom + "' to '" + currencyTo + "': " + ex.getMessage());
             return 0.0;
         }
+    }
+
+    public HashMap<String, String> getAllCurrenciesInfo() {
+        return allCurrenciesInfo;
+    }
+
+    public void setAllCurrenciesInfo(HashMap<String, String> allCurrenciesInfo) {
+        this.allCurrenciesInfo = allCurrenciesInfo;
+    }
+
+    public HashMap<String, Double> getExchangeRateInfo() {
+        return exchangeRateInfo;
+    }
+
+    public void setExchangeRateInfo(HashMap<String, Double> exchangeRateInfo) {
+        this.exchangeRateInfo = exchangeRateInfo;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
     }
 }
