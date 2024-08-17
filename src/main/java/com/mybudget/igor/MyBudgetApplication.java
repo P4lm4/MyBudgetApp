01574cdaa17d7da2f36355b459dbad5108f661d4
Igor Pavlović <pigorp@hotmail.com>
2024-08-17 23:23:42 +0200

    Created auto XML importing on start up.

--- src/main/java/com/mybudget/igor/MyBudgetApplication.java
+++ src/main/java/com/mybudget/igor/MyBudgetApplication.java
@@ -1,13 +1,40 @@
 package com.mybudget.igor;
 
+import com.mybudget.igor.model.XmlImporter;
+import com.mybudget.igor.service.AccountService;
+import com.mybudget.igor.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 @SpringBootApplication
 public class MyBudgetApplication {
+	@Autowired
+	private AccountService accountService;
+	@Autowired
+	private TransactionService transactionService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBudgetApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return args -> {
+
+			File xmlFile = new File("../my_budget_data.xml");
+			if(xmlFile.exists()) {
+				System.out.println("XML import start.");
+				XmlImporter.importXml("../my_budget_data.xml", accountService, transactionService);
+				System.out.println("XML file imported.");
+				xmlFile.renameTo(new File("../my_budget_data_IMPORTED.xml"));
+			} else {
+				System.out.println("No xml file to import.");
+			}
+		};
+	}
 }
---
+++ src/main/java/com/mybudget/igor/model/XmlImporter.java
@@ -0,0 +1,80 @@
+package com.mybudget.igor.model;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import com.mybudget.igor.service.AccountService;
+import com.mybudget.igor.service.TransactionService;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class XmlImporter {
+
+    public static void importXml(String fileName, AccountService accountService, TransactionService transactionService) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(fileName);
+
+            doc.getDocumentElement().normalize();
+
+            Element accountsRoot = (Element) doc.getElementsByTagName("Accounts").item(0);
+            NodeList accountList = accountsRoot.getElementsByTagName("Account");
+
+            for(int i = 0; i < accountList.getLength(); i++) {
+                Node accountNode = accountList.item(i);
+
+                if(accountNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element accountElement = (Element) accountNode;
+                    Account newAccount = new Account();
+
+                    newAccount.setName(accountElement.getAttribute("name"));
+                    newAccount.setCurrency(accountElement.getAttribute("currency").toLowerCase());
+
+                    String balanceText = accountElement.getElementsByTagName("Balance").item(0).getTextContent();
+                    Double balance = Double.parseDouble(balanceText);
+                    newAccount.setBalance(balance);
+
+                    accountService.addAccount(newAccount);
+
+                    Element transactionsRoot = (Element) accountElement.getElementsByTagName("Transactions").item(0);
+                    if(transactionsRoot == null) {
+                        continue;
+                    }
+                    NodeList transactionList = transactionsRoot.getElementsByTagName("Transaction");
+
+                    for(int j = 0; j <transactionList.getLength(); j++) {
+                        Node transactionNode = transactionList.item(j);
+
+                        if(transactionNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element transactionElement = (Element) transactionNode;
+                            Transaction newTransaction = new Transaction();
+
+                            newTransaction.setAccount(newAccount);
+                            newTransaction.setDescription(transactionElement.getElementsByTagName("Description").item(0).getTextContent());
+                            Element amountElement = (Element) transactionElement.getElementsByTagName("Amount").item(0);
+                            String amountText = amountElement.getTextContent();
+                            Double amount = Double.parseDouble(amountText);
+                            if(amount >= 0) {
+                                newTransaction.setType(TransactionType.INCOME);
+                            } else {
+                                newTransaction.setType(TransactionType.EXPENSE);
+                                amount *= -1;
+                            }
+                            newTransaction.setAmount(amount);
+                            newTransaction.setCurrency(amountElement.getAttribute("currency").toLowerCase());
+
+                            transactionService.addTransaction(newTransaction);
+
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to import XML: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
